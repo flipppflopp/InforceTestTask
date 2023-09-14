9f34d6e648685cf1372344a5be0f69c977c61ae6
flipppflopp <62306692+flipppflopp@users.noreply.github.com>
2023-09-14 15:00:53 +0300

    Added valid unit tests for services
    unit tests

---
+++ Tests/Services/URLServiceTests.cs
@@ -0,0 +1,130 @@
+﻿using DB.Context;
+using DB.Models;
+using Microsoft.EntityFrameworkCore;
+using Services.Repository;
+using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Tests
+{
+    public class URLServiceTests : IDisposable
+    {
+        private ApplicationContext _context;
+        private URLService _urlService;
+
+        public URLServiceTests()
+        {
+            var guid = Guid.NewGuid().ToString();
+            var options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase(guid)
+                .Options;
+            _context = new ApplicationContext(options);
+            _urlService = new URLService(_context);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        [Fact]
+        public void Get_ReturnsAllURLs()
+        {
+            // Arrange
+            var urls = new List<URL>
+            {
+                new URL { ID = 1, DefaultURL = "http://example1.com", ShourtedURL = "" },
+                new URL { ID = 2, DefaultURL = "http://example2.com", ShourtedURL = "" },
+            };
+            _context.URLs.AddRange(urls);
+            _context.SaveChanges();
+
+            // Act
+            var result = _urlService.Get();
+
+            // Assert
+            Assert.Equal(urls, result);
+        }
+
+        [Fact]
+        public void Get_ReturnsURLById()
+        {
+            // Arrange
+            var url = new URL { ID = 1, DefaultURL = "http://example.com", ShourtedURL = "" };
+            _context.URLs.Add(url);
+            _context.SaveChanges();
+
+            // Act
+            var result = _urlService.Get(1);
+
+            // Assert
+            Assert.Equal(url, result);
+        }
+
+        [Fact]
+        public void Post_CreatesNewURL()
+        {
+            // Arrange
+            var newURL = new URL { DefaultURL = "http://newexample.com", ShourtedURL = "" };
+
+            // Act
+            var result = _urlService.Post(newURL);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("http://newexample.com", result.DefaultURL);
+        }
+
+        [Fact]
+        public void Post_ReturnsNullForDuplicateURL()
+        {
+            // Arrange
+            var existingURL = new URL { DefaultURL = "http://existing.com", ShourtedURL = "" };
+            _context.URLs.Add(existingURL);
+            _context.SaveChanges();
+            var duplicateURL = new URL { DefaultURL = "http://existing.com", ShourtedURL = "" };
+
+            // Act
+            var result = _urlService.Post(duplicateURL);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Put_UpdatesURL()
+        {
+            // Arrange
+            var url = new URL { ID = 1, DefaultURL = "http://example.com", ShourtedURL = "" };
+            _context.URLs.Add(url);
+            _context.SaveChanges();
+            url.DefaultURL = "http://updatedexample.com";
+
+            // Act
+            _urlService.Put(url);
+            var updatedURL = _context.URLs.Find(1);
+
+            // Assert
+            Assert.Equal("http://updatedexample.com", updatedURL.DefaultURL);
+        }
+
+        [Fact]
+        public void Delete_RemovesURL()
+        {
+            // Arrange
+            var url = new URL { ID = 1, DefaultURL = "http://example.com", ShourtedURL = ""};
+            _context.URLs.Add(url);
+            _context.SaveChanges();
+
+            // Act
+            _urlService.Delete(1);
+            var deletedURL = _context.URLs.Find(1);
+
+            // Assert
+            Assert.Null(deletedURL);
+        }
+    }
+}
---
+++ Tests/Services/UserServiceTests.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DB.Context;
+using DB.Models;
+using Microsoft.EntityFrameworkCore;
+using Services.Repository;
+using Services.Services;
+using Xunit;
+
+namespace Tests
+{
+    public class UserServiceTests : IDisposable
+    {
+        private readonly ApplicationContext _context;
+        private readonly UserService _userService;
+
+        public UserServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            _context = new ApplicationContext(options);
+            _userService = new UserService(_context);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        [Fact]
+        public void Get_ReturnsAllUsers()
+        {
+            // Arrange
+            var users = new List<User>
+            {
+                new User { ID = 1, Login = "user1", Password = "password1" },
+                new User { ID = 2, Login = "user2", Password = "password2" }
+            };
+            _context.Users.AddRange(users);
+            _context.SaveChanges();
+
+            // Act
+            var result = _userService.Get();
+
+            // Assert
+            Assert.Equal(users, result);
+        }
+
+        [Fact]
+        public void Get_ReturnsUserById()
+        {
+            // Arrange
+            var user = new User { ID = 1, Login = "user1", Password = "password1" };
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            // Act
+            var result = _userService.Get(1);
+
+            // Assert
+            Assert.Equal(user, result);
+        }
+
+        [Fact]
+        public void Validate_ReturnsUserForValidCredentials()
+        {
+            // Arrange
+            var user = new User { ID = 1, Login = "user1", Password = "password1" };
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            var inputUser = new User { Login = "user1", Password = "password1" };
+
+            // Act
+            var result = _userService.Validate(inputUser);
+
+            // Assert
+            Assert.Equal(user, result);
+        }
+
+        [Fact]
+        public void Validate_ReturnsNullForInvalidCredentials()
+        {
+            // Arrange
+            var user = new User { ID = 1, Login = "user1", Password = "password1" };
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            var inputUser = new User { Login = "user1", Password = "wrongpassword" };
+
+            // Act
+            var result = _userService.Validate(inputUser);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Post_CreatesNewUser()
+        {
+            // Arrange
+            var newUser = new User { Login = "newuser", Password = "newpassword" };
+
+            // Act
+            _userService.Post(newUser);
+
+            // Assert
+            var createdUser = _context.Users.FirstOrDefault(u => u.Login == "newuser");
+            Assert.NotNull(createdUser);
+        }
+
+        [Fact]
+        public void Put_UpdatesUser()
+        {
+            // Arrange
+            var user = new User { ID = 1, Login = "user1", Password = "password1" };
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            user.Password = "newpassword";
+
+            // Act
+            _userService.Put(user);
+
+            // Assert
+            var updatedUser = _context.Users.Find(1);
+            Assert.Equal("newpassword", updatedUser.Password);
+        }
+
+        [Fact]
+        public void Delete_RemovesUser()
+        {
+            // Arrange
+            var user = new User { ID = 1, Login = "user1", Password = "password1" };
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            // Act
+            _userService.Delete(user);
+
+            // Assert
+            var deletedUser = _context.Users.Find(1);
+            Assert.Null(deletedUser);
+        }
+    }
+}
